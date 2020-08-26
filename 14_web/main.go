1ef41de7427f63bccdf0de7e02a23da43f1decbd
PM <pedromendonka@users.noreply.github.com>
2020-08-26 17:02:52 +0100

    web

--- 14_web/main.go
+++ 14_web/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(res http.ResponseWriter, req http.Request) {
+	fmt.Fprint(w)
+}
 
 func main() {
 	fmt.Println("WEB")
+
+	http.HandleFunc("/", index)
 }
